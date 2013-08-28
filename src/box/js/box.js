441b4e6175d763e15eaa5ec260cf69dee2f4e6b4
Roman Tsisyk <roman@tsisyk.com>
2013-08-28 15:05:22 +0400

    [js] Bindigns for box.info, .cfg, .slab, .stat

--- src/box/js/box.js
+++ src/box/js/box.js
@@ -1,1 +1,9 @@
 /* Alter 'exports' object here */
+(function() {
+    var lua = require('lua')
+    /* Fallback to Lua for these bindings */
+    exports.cfg = lua.box.cfg
+    exports.info = lua.box.info
+    exports.slab = lua.box.slab
+    exports.stat = lua.box.stat
+})();
