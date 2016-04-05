e043c7c63aae6b8ad46bff9d0cd1e1447bb7dfad
Vladislav Shpilevoy <v.shpilevoy@corp.mail.ru>
2016-04-05 18:11:36 +0300

    Update tcl tests
    lua scripts don't drop tables - it is
    making now in tcl.
    Types were fixed - to integer from real in
    tests results.

--- test/tcl/tclselect1.test.lua
+++ test/tcl/tclselect1.test.lua
@@ -4,21 +4,6 @@ tarantool_path = io.popen("which tarantool", "r"):read()
 
 require("top")
 
--- init space test
-if box.space.test then
-    box.space.test:drop()
-end
-
--- init space test2 
-if box.space.test2 then
-    box.space.test2:drop()
-end
-
--- init space test3
-if box.space.test3 then
-	box.space.test3:drop()
-end
-
 -- initialization params
 local arg0 = ffi.cast('char *',  tarantool_path)
 local arg1 = ffi.cast('char *', "./select1.test")
