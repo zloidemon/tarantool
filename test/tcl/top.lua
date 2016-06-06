a6fc6021f848bde73a239b54661d893c03190f6e
Vladislav Shpilevoy <v.shpilevoy@corp.mail.ru>
2016-06-06 19:39:19 +0300

    Fix testfixture name in tcl tests

--- test/tcl/top.lua
+++ test/tcl/top.lua
@@ -20,7 +20,7 @@ require('console').listen(os.getenv('ADMIN'))
 ffi = require('ffi')    
 
 -- loading textfixture - dynamic librarary for running tests
-fixture = ffi.load( '../../third_party/sqlite/src/libtestfixture.so')
+fixture = ffi.load( '../../third_party/sqlite/src/libtestfixture.dylib')
 
 -- define function
 ffi.cdef('int main(int argc, char *argv[])')
