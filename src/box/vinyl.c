4b1deb4fc7653414c991d9d25f1d52d340507ea0
Vladimir Davydov <vdavydov.dev@gmail.com>
2016-09-12 15:09:18 +0300

    Revert "vinyl: don't take index ref in vy_cursor_new and vy_index_send"
    This reverts commit a283ed3ee97270ae8fc3fd5c9291eef382002444.
    
    vy_cursor_delete() easily hits use-after-free with this patch. Let's
    revert it until proper index reference counting is implemented at the
    upper level.
    
    Conflicts:
    	src/box/vinyl.c

--- src/box/vinyl.c
+++ src/box/vinyl.c
@@ -5279,6 +5279,7 @@ vy_cursor_new(struct vy_tx *tx, struct vy_index *index, const char *key,
 	c->index = index;
 	c->n_reads = 0;
 	c->order = order;
+	vy_index_ref(index);
 	if (tx == NULL) {
 		tx = &c->tx_autocommit;
 		vy_tx_begin(e->xm, tx, VINYL_TX_RO);
@@ -5308,6 +5309,7 @@ vy_cursor_delete(struct vy_cursor *c)
 	}
 	if (c->key)
 		vy_tuple_unref(c->key);
+	vy_index_unref(c->index);
 	vy_stat_cursor(e->stat, c->start, c->n_reads);
 	TRASH(c);
 	mempool_free(&e->cursor_pool, c);
@@ -6565,6 +6567,8 @@ vy_index_send(struct vy_index *index, vy_send_row_f sendrow, void *ctx)
 	int64_t vlsn = INT64_MAX;
 	int rc = 0;
 
+	vy_index_ref(index);
+
 	struct svmerge merge;
 	sv_mergeinit(&merge, index, index->key_def);
 	struct vy_rangeiter range_iter;
@@ -6619,6 +6623,7 @@ vy_index_send(struct vy_index *index, vy_send_row_f sendrow, void *ctx)
 	}
 finish_send:
 	sv_mergefree(&merge);
+	vy_index_unref(index);
 	return rc;
 }
 
